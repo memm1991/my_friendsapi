5dec72c3cf44bc2bebd9e24c9da1ed028ade538c
memm1991 <migueledmejail@gmail.com>
2021-03-27 14:10:43 -0300

    app friends

---
+++ index.js
@@ -0,0 +1,24 @@
+const http =require('http')
+
+const server = http.createServer((request,response)=>{
+    console.log('ruta' + request.url)
+    switch(request.url){
+        case '/':
+             response.writeHead(200, {'Content_type':'application/json'})
+            // response.statusCode(200)
+            // response.statusMessage('que onda')
+            response.end(JSON.stringify('Hola mundo'))
+            break;
+        default:
+            response.writeHead(404, {'Content_type':'text/plain'})
+            // response.statusCode(404) 
+            response.end('no hay pagina')   
+
+
+
+    }
+})
+
+server.listen(3000, ()=>{
+    console.log('escuchando en el puerto 3000')
+})
